ada35bd29c62e6ec94f66b6521e5ae6f6c0a6901
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-04 17:43:42 -0400

    pantalla de carga agregada

---
+++ src/app/loading.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default async function Loading() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+      <Image
+        src="/assets/images/noBgBlack.png"
+        alt="Cargando-Devhoo..."
+        width={200}
+        height={100}
+        className="animate-pulse"
+      />
+    </div>
+  )
+
+}

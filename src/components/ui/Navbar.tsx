f35e5fa307f6f1d1cda60b6d3c88e2e1e0a624cb
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-04 12:02:46 -0400

    configuracion navbar para version mobile

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,6 +1,15 @@
+"use client";
+
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+
 import Image from "next/image";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <header className="w-full bg-purple-300 border-b border-gray-300 fixed top-0 left-0 z-50">
       <div className="max-w-7xl mx-auto px-6 py-2 flex justify-between items-center">
@@ -13,6 +22,7 @@ export default function Navbar() {
           />
         </div>
 
+        {/* Menu para escritorio */}
         <nav className="hidden md:flex gap-6 text-gray-950 font-medium">
           <a className="hover:text-gray-100 transition">Inicio</a>
           <a className="hover:text-gray-100 transition">Servicios</a>
@@ -20,7 +30,28 @@ export default function Navbar() {
           <a className="hover:text-gray-100 transition">Nosotros</a>
           <a className="hover:text-gray-100 transition">Contactanos</a>
         </nav>
+
+        {/* Boton hamburguesa para mobile */}
+        <button
+          className="md:hidden text-gray-700 focus:outline-none"
+          onClick={toggleMenu}
+        >
+          {isOpen ?< X size={28} /> : <Menu size={28} />}
+        </button>
       </div>
+
+      {/* Menu mobile */}
+      {isOpen && (
+        <nav className="md:hidden bg-purple-300 border-t border-gray-300 shadow-md">
+          <div className="flex flex-col items-center py-4 space-y-4">
+            <a onClick={toggleMenu} className="hover:text-blue-600 transition">Servicios</a>
+            <a onClick={toggleMenu} className="hover:text-blue-600 transition">Portafolio</a>
+            <a onClick={toggleMenu} className="hover:text-blue-600 transition">Nosotros</a>
+            <a onClick={toggleMenu} className="hover:text-blue-600 transition">Contacto</a>
+          </div>
+        </nav>
+      )}
+
     </header>
   )
 }

facdfe488d1525eb4c6d51ea01a7428078991e7e
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-06 14:56:53 -0400

    uso del componente slide en la seccion servicios

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,14 +1,7 @@
+import ServicesSection from "@/components/ui/ServicesSection";
+
 export default function Services() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-8">
-      <h1 className="text-3xl font-bold text-center">
-        Servicios
-      </h1>
-      <p className="text-gray-600 text-center max-w-xl">
-        Seccion para mostrar los servicios que ofrece 
-        la landing page de Devhoo
-      </p>
-
-    </div>
+    <ServicesSection/>
   )
 }

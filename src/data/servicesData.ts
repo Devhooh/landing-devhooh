f3b7ba5cbc1c10fc990f4e30582f59078b28bdb7
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-12 12:18:57 -0400

    datos agregados para los servicios

---
+++ src/data/servicesData.ts
@@ -0,0 +1,142 @@
+// Card data interface
+interface CardData {
+  title: string;
+  details: string[];
+  color: "indigo" | "teal" | "orange";
+  icon: string;
+}
+
+// Datos de la Card
+export const customDevelopmentCardData: CardData[] = [
+  {
+    title: "Desarrollo web",
+    details: [
+      "Diseño único, ideal para negocios, tiendas online, portafolios y sistemas a medida.",
+      "Creamos tu tienda online con un diseño atractivo y fácil de usar desde el primer día.",
+      "Diseño y desarrollo web, desde el boceto hasta el lanzamiento oficial."
+    ],
+    color: "indigo",
+    icon: "/assets/images/cardImg.png",
+  },
+  {
+    title: "Desarrollo Mobile",
+    details: [
+      "Desarrollo de apps móviles nativas y multiplataforma para iOS y Android.",
+      "Apps personalizadas que impulsan tu negocio y mejoran la experiencia del usuario.",
+      "Creamos tu app con enfoque en experiencia, rendimiento y escalabilidad."
+    ],
+    color: "orange",
+    icon: "/assets/images/cardImg.png",
+  },
+  {
+    title: "Desarrollo Software",
+    details: [
+      "Creamos soluciones a medida para automatizar, gestionar o escalar tu negocio.",
+      "Soluciones de software personalizadas que se adaptan a tus necesidades.",
+      "Tu operación más eficiente, con un desarrollo adaptado 100% a tus necesidades."
+    ],
+    color: "teal",
+    icon: "/assets/images/cardImg.png",
+  },
+];
+
+export const ServicesIACardData: CardData[] = [
+  {
+    title: "Desarrollo web",
+    details: [
+      "Diseño único, ideal para negocios, tiendas online, portafolios y sistemas a medida.",
+      "Creamos tu tienda online con un diseño atractivo y fácil de usar desde el primer día.",
+      "Diseño y desarrollo web, desde el boceto hasta el lanzamiento oficial."
+    ],
+    color: "indigo",
+    icon: "/assets/images/cardImg.png",
+  },
+  {
+    title: "Desarrollo Mobile",
+    details: [
+      "Desarrollo de apps móviles nativas y multiplataforma para iOS y Android.",
+      "Apps personalizadas que impulsan tu negocio y mejoran la experiencia del usuario.",
+      "Creamos tu app con enfoque en experiencia, rendimiento y escalabilidad."
+    ],
+    color: "orange",
+    icon: "/assets/images/cardImg.png",
+  },
+  {
+    title: "Desarrollo Software",
+    details: [
+      "Creamos soluciones a medida para automatizar, gestionar o escalar tu negocio.",
+      "Soluciones de software personalizadas que se adaptan a tus necesidades.",
+      "Tu operación más eficiente, con un desarrollo adaptado 100% a tus necesidades."
+    ],
+    color: "teal",
+    icon: "/assets/images/cardImg.png",
+  },
+];
+
+export const InfraestructureSupportCardData: CardData[] = [
+  {
+    title: "Desarrollo web",
+    details: [
+      "Diseño único, ideal para negocios, tiendas online, portafolios y sistemas a medida.",
+      "Creamos tu tienda online con un diseño atractivo y fácil de usar desde el primer día.",
+      "Diseño y desarrollo web, desde el boceto hasta el lanzamiento oficial."
+    ],
+    color: "indigo",
+    icon: "/assets/images/cardImg.png",
+  },
+  {
+    title: "Desarrollo Mobile",
+    details: [
+      "Desarrollo de apps móviles nativas y multiplataforma para iOS y Android.",
+      "Apps personalizadas que impulsan tu negocio y mejoran la experiencia del usuario.",
+      "Creamos tu app con enfoque en experiencia, rendimiento y escalabilidad."
+    ],
+    color: "orange",
+    icon: "/assets/images/cardImg.png",
+  },
+  {
+    title: "Desarrollo Software",
+    details: [
+      "Creamos soluciones a medida para automatizar, gestionar o escalar tu negocio.",
+      "Soluciones de software personalizadas que se adaptan a tus necesidades.",
+      "Tu operación más eficiente, con un desarrollo adaptado 100% a tus necesidades."
+    ],
+    color: "teal",
+    icon: "/assets/images/cardImg.png",
+  },
+];
+
+export const DesignerQACardData: CardData[] = [
+  {
+    title: "Desarrollo web",
+    details: [
+      "Diseño único, ideal para negocios, tiendas online, portafolios y sistemas a medida.",
+      "Creamos tu tienda online con un diseño atractivo y fácil de usar desde el primer día.",
+      "Diseño y desarrollo web, desde el boceto hasta el lanzamiento oficial."
+    ],
+    color: "indigo",
+    icon: "/assets/images/cardImg.png",
+  },
+  {
+    title: "Desarrollo Mobile",
+    details: [
+      "Desarrollo de apps móviles nativas y multiplataforma para iOS y Android.",
+      "Apps personalizadas que impulsan tu negocio y mejoran la experiencia del usuario.",
+      "Creamos tu app con enfoque en experiencia, rendimiento y escalabilidad."
+    ],
+    color: "orange",
+    icon: "/assets/images/cardImg.png",
+  },
+  {
+    title: "Desarrollo Mobile",
+    details: [
+      "Desarrollo de apps móviles nativas y multiplataforma para iOS y Android.",
+      "Apps personalizadas que impulsan tu negocio y mejoran la experiencia del usuario.",
+      "Creamos tu app con enfoque en experiencia, rendimiento y escalabilidad."
+    ],
+    color: "orange",
+    icon: "/assets/images/cardImg.png",
+  },
+  
+];
+

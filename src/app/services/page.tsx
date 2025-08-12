af0a7ea44086b99d604544e90de6b72393d66e1e
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-12 00:04:37 -0400

    div para para los componentes de servicios ofrecidos

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,3 +1,4 @@
+import CustomDevelopmentSection from "@/components/ui/CustomDevelopmentSection";
 import ServicesCardsSection from "@/components/ui/ServicesCardsSection";
 import ServicesSection from "@/components/ui/ServicesSection";
 
@@ -6,6 +7,20 @@ export default function Services() {
     <div className="flex flex-col gap-12">
       <ServicesSection/>
       <ServicesCardsSection/>
+
+      <div className="w-full py-10 bg-indigo-950/90 text-white overflow-hidden">
+        <div className="max-w-7xl mx-auto px-6 space-y-10">
+          <div className="flex items-center justify-center p-4 rounded-2xl bg-sky-400">
+            <h3 className="font-extrabold text-3xl text-center">
+              Servicios que ofrecemos
+            </h3>
+          </div>
+
+          <CustomDevelopmentSection/>
+        </div>
+      </div>
+
+
     </div>
 
   )

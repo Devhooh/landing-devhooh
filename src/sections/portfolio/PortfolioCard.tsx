16673ca2e6b6055e192a29d28bdadc3fdfc18606
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-13 17:10:25 -0400

    se agrego el lado inverso de la card para detalles

--- src/sections/portfolio/PortfolioCard.tsx
+++ src/sections/portfolio/PortfolioCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 
 interface Project {
@@ -6,44 +8,63 @@ interface Project {
   country: string;
   service: string;
   technologies: string[];
+  description: string;
 }
 
 export default function PortfolioCard({ project }: { project: Project }) {
-  return (
-    <div className="bg-white rounded-lg shadow-xl overflow-hidden transform transition-transform duration-300 hover:scale-105">
-      <div className="bg-gray-200 p-3 h-48 w-full flex items-center justify-center">
-        <Image
-          width={200}
-          height={200}
-          src="/assets/images/slide1.png"
-          alt={project.title}
-          className="object-contain w-full h-full"
-        />
-      </div>
+  const [flipped, setFlipped] = useState(false);
 
-      <div className="p-6">
-        <h3 className="text-xl font-semibold text-gray-800 mb-2">
-          {project.title}
-        </h3>
-        <div className="text-sm text-gray-500 mb-4">
-          <p>
-            <span className="font-medium">Servicio:</span> {project.service}
-          </p>
-        </div>
-        <div className="flex flex-wrap gap-2">
-          <div className="text-sm text-gray-500">
-            <p>
-              <span className="font-medium">Tecnologias:</span>
-            </p>
+  return (
+    <div
+      className="relative w-full h-[350px] cursor-pointer perspective"
+      onClick={() => setFlipped(!flipped)}
+    >
+      {/* Contenedor para rotación */}
+      <div
+        className={`relative w-full h-full transition-transform duration-700 transform-style-preserve-3d ${
+          flipped ? "rotate-y-180" : ""
+        }`}
+      >
+        {/* Lado frontal */}
+        <div className="absolute w-full h-full backface-hidden bg-white rounded-lg shadow-xl overflow-hidden">
+          <div className="bg-gray-200 p-3 h-48 w-full flex items-center justify-center">
+            <Image
+              width={200}
+              height={200}
+              src="/assets/images/slide1.png"
+              alt={project.title}
+              className="object-contain w-full h-full"
+            />
           </div>
-          {project.technologies.map((tech) => (
-            <span
-              key={tech}
-              className="bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded-full"
-            >
-              {tech}
-            </span>
-          ))}
+          <div className="p-6">
+            <h3 className="text-xl font-semibold text-gray-800 mb-2">
+              {project.title}
+            </h3>
+            <p className="text-sm text-gray-500">
+              <span className="font-medium">Servicio:</span> {project.service}
+            </p>
+            <div className="flex flex-wrap gap-2 mt-2">
+              {project.technologies.map((tech) => (
+                <span
+                  key={tech}
+                  className="bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded-full"
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Lado trasero */}
+        <div className="absolute w-full h-full bg-indigo-900 text-white p-6 rounded-lg shadow-xl backface-hidden rotate-y-180 flex flex-col justify-between">
+          <h3 className="text-2xl font-semibold text-white mb-2 items-center text-center">
+            Descripcion
+          </h3>
+          <p>{project.description}</p>
+          <button className="bg-white text-indigo-900 font-bold py-2 px-4 rounded-lg mt-4 hover:bg-gray-300 transform transition">
+            Ver más detalles
+          </button>
         </div>
       </div>
     </div>

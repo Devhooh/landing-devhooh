316b37af02e2a2a55ed7cfe65dca58c5da752d3b
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-12 22:38:13 -0400

    configuracion del border

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
 
 
   return (
-    <footer className="w-full bg-fuchsia-950 mt-8 p-2 border-t border-gray-200">
+    <footer className="w-full bg-fuchsia-950 p-4 border-t ">
       <div className="max max-w-7xl mx-auto p-5 grid grid-cols-1 table-lg:grid-cols-2 gap-8">
         {/* Lado izquierdo */}
         <div>

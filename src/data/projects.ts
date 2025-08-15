a1a8cdb6b6d781b8ebec8d0870965d326c27729c
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-15 11:18:14 -0400

    data para pruebas

---
+++ src/data/projects.ts
@@ -0,0 +1,51 @@
+// src/data/projects.ts
+
+export const logos = [
+  { src: "/assets/images/BgColor.png", alt: "Logo 1" },
+  { src: "/assets/images/noBgBlack.png", alt: "Logo 2" },
+  { src: "/assets/images/noBgWhite.png", alt: "Logo 3" },
+  { src: "/assets/images/noBgColor.png", alt: "Logo 4" },
+  { src: "/assets/images/BgColor.png", alt: "Logo 5" },
+  { src: "/assets/images/noBgBlack.png", alt: "Logo 6" },
+  { src: "/assets/images/noBgWhite.png", alt: "Logo 7" },
+  { src: "/assets/images/noBgColor.png", alt: "Logo 8" },
+];
+
+export const projects = [
+  {
+    image: "/assets/images/slide1.png",
+    title: "Plataforma de e-commerce",
+    subtitle: "Aumentamos un 45% sus ventas en 3 meses.",
+    link: "/projects/ecommerce",
+  },
+  {
+    image: "/assets/images/slide1.png",
+    title: "App de delivery",
+    subtitle: "Optimización del 30% en tiempos de entrega.",
+    link: "/projects/delivery",
+  },
+  {
+    image: "/assets/images/slide1.png",
+    title: "Sistema de reservas",
+    subtitle: "Reducimos un 50% los tiempos de espera de clientes.",
+    link: "/projects/reservas",
+  },
+  {
+    image: "/assets/images/slide1.png",
+    title: "Plataforma de e-commerce",
+    subtitle: "Aumentamos un 45% sus ventas en 3 meses.",
+    link: "/projects/ecommerce",
+  },
+  {
+    image: "/assets/images/slide1.png",
+    title: "App de delivery",
+    subtitle: "Optimización del 30% en tiempos de entrega.",
+    link: "/projects/delivery",
+  },
+  {
+    image: "/assets/images/slide1.png",
+    title: "Sistema de reservas",
+    subtitle: "Reducimos un 50% los tiempos de espera de clientes.",
+    link: "/projects/reservas",
+  },
+];

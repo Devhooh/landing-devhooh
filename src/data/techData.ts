062b77fed8952da6b405828bceb656a000fbd0b4
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-08-16 22:29:19 -0400

    datos de logos y nombres

---
+++ src/data/techData.ts
@@ -0,0 +1,96 @@
+import { BiLogoPostgresql, BiLogoGoLang  } from "react-icons/bi";
+import { TbBrandReactNative, TbBrandBlender } from "react-icons/tb";
+import { TbBrandCSharp } from "react-icons/tb";
+import { DiDjango } from "react-icons/di";
+import { PiFigmaLogoFill } from "react-icons/pi";
+
+import { 
+  FaJsSquare, 
+  FaNode, 
+  FaReact, 
+  FaAngular, 
+  FaPython,
+  FaSwift,
+  FaJava,
+  FaGitAlt,
+  FaGithub,
+  FaDocker,
+  FaAws,
+  FaSketch 
+} from "react-icons/fa";
+
+import { 
+  SiMongodb, 
+  SiNextdotjs, 
+  SiTailwindcss, 
+  SiTypescript, 
+  SiVite, 
+  SiNestjs, 
+  SiMysql,
+  SiKotlin,
+  SiFlutter,
+  SiVercel,
+  SiKubernetes,
+  SiExpress,
+  SiDotnet,
+  SiCplusplus,
+  SiRust,
+  SiAdobexd
+} from "react-icons/si";
+
+
+export const frontendTech = [
+  { icon: FaJsSquare, name: "JavaScript", color: "text-yellow-400" },
+  { icon: SiTypescript, name: "TypeScript", color: "text-blue-600" },
+  { icon: SiTailwindcss, name: "TailwindCSS", color: "text-sky-500" },
+  { icon: SiNextdotjs, name: "Next.js", color: "text-gray-900" },
+  { icon: FaAngular, name: "Angular", color: "text-red-600" },
+  { icon: FaReact, name: "React", color: "text-cyan-400" },
+];
+
+export const backendTech = [
+  { icon: FaNode, name: "NodeJs", color: "text-green-400" },
+  { icon: SiMysql, name: "MySQL", color: "text-sky-500" },
+  { icon: SiMongodb, name: "MongoDB", color: "text-green-600" },
+  { icon: SiNestjs, name: "NestJs", color: "text-red-600" },
+  { icon: BiLogoPostgresql, name: "PostgreSQL", color: "text-blue-600" },
+  { icon: SiExpress, name: "ExpressJs", color: "text-gray-700" },
+];
+
+export const mobilTech = [
+  { icon: SiKotlin, name: "Kotlin", color: "text-fuchsia-500" },
+  { icon: SiFlutter, name: "Flutter", color: "text-sky-500" },
+  { icon: FaSwift, name: "Swift", color: "text-orange-400" },
+  { icon: TbBrandReactNative, name: "ReactNative", color: "text-blue-600" },
+];
+
+export const softwareTech = [
+  { icon: SiDotnet, name: "Dotnet", color: "text-fuchsia-500" },
+  { icon: TbBrandCSharp, name: "CSharp", color: "text-purple-800" },
+  { icon: FaPython, name: "Python", color: "text-blue-600" },
+  { icon: FaJava, name: "Java", color: "text-blue-800" },
+  { icon: DiDjango, name: "Django", color: "text-green-800" },
+  { icon: BiLogoGoLang, name: "GO", color: "text-blue-800" },
+  { icon: SiCplusplus, name: "C++", color: "text-blue-500" },
+  { icon: SiRust, name: "Rust", color: "text-black" },
+];
+
+export const iaTech = [
+
+];
+
+export const infraTech = [
+  { icon: FaDocker, name: "Docker", color: "text-sky-400" },
+  { icon: SiKubernetes, name: "Kubernetes", color: "text-blue-800" },
+  { icon: FaAws, name: "FaAws", color: "text-orange-600" },
+  { icon: FaGithub, name: "Github", color: "text-black-800" },
+  { icon: FaGitAlt, name: "Git", color: "text-red-500" },
+  { icon: SiVercel, name: "Vercel", color: "text-black-800" },
+  { icon: SiVite, name: "Vite", color: "text-purple-500" },
+];
+export const designerTech = [
+  { icon: PiFigmaLogoFill, name: "Figma", color: "text-gray-700" },
+  { icon: SiAdobexd, name: "Adobexd", color: "text-fuchsia-900" },
+  { icon: TbBrandBlender, name: "Blender", color: "text-orange-500" },
+  { icon: FaSketch, name: "Sketch ", color: "text-yellow-500" },
+];

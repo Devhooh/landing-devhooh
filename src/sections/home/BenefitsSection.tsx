e519eba084713245a9e925438a4910f76dcf792c
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-09-07 15:05:38 -0400

    correccion del desbordamiento de la img

--- src/sections/home/BenefitsSection.tsx
+++ src/sections/home/BenefitsSection.tsx
@@ -153,7 +153,7 @@ const checklistItems = [
           {/* BLOQUE DE IMAGEN */}
           <motion.div
             className="relative flex justify-center table-lg:justify-end mx-5"
-            initial={{ opacity: 0, x: 50 }}
+            initial={{ opacity: 0, x: 20 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8 }}
             viewport={{ once: true }}

6378ae0f44b437dbc21d66c03df0debd71c57479
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-09-18 10:56:28 -0400

    se agregaron las imagenes a la data de servicios

--- src/data/servicesData.ts
+++ src/data/servicesData.ts
@@ -62,13 +62,13 @@ export const InfraestructureSupportCardData: CardData[] = [
     slug: "despliegue",
     title: "Despliegue de Aplicaciones",
     details: "Nos encargamos del despliegue seguro y eficiente de tus aplicaciones en entornos locales o en la nube, asegurando escalabilidad, estabilidad y continuidad operativa.",
-    icon: "/assets/images/cardImg.png",
+    icon: "/assets/images/despliegue.png",
   },
   {
     slug: "infraestructura",
     title: "Infraestructura",
     details: "Diseñamos, implementamos y gestionamos infraestructuras tecnológicas sólidas y escalables, adaptadas a las necesidades de tu negocio y listas para crecer contigo.",
-    icon: "/assets/images/cardImg.png",
+    icon: "/assets/images/infraestructura.png",
   },
 ];
 
@@ -77,7 +77,7 @@ export const DesignerQACardData: CardData[] = [
     slug: "diseno",
     title: "Diseño UX/UI",
     details: "Creamos experiencias digitales atractivas y funcionales con un diseño UX/UI enfocado en usabilidad, accesibilidad y conversión, mejorando la interacción de los usuarios con tu producto.",
-    icon: "/assets/images/cardImg.png",
+    icon: "/assets/images/design.png",
   },
   // {
   //   slug: "investigacion",
@@ -89,7 +89,7 @@ export const DesignerQACardData: CardData[] = [
     slug: "qa",
     title: "QA",
     details: "Implementamos pruebas de calidad (QA) para asegurar que tus aplicaciones funcionen sin errores, con alto rendimiento y una experiencia confiable en todas las plataformas.",
-    icon: "/assets/images/cardImg.png",
+    icon: "/assets/images/qa.png",
   },
 ];
 

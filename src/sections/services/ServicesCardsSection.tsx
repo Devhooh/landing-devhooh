d3ba744e6b214655f6022e950bc5185c7426d81a
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-10-23 18:15:25 -0400

    componente refactorizado para no usar use client

--- src/sections/services/ServicesCardsSection.tsx
+++ src/sections/services/ServicesCardsSection.tsx
@@ -1,23 +1,6 @@
-"use client";
-
 import { Lightbulb, Rocket, Repeat, Sparkles, Target, Zap } from "lucide-react";
-import { motion, Variants } from "framer-motion";
 import Link from "next/link";
-
-// Variantes para animaciones
-const fadeRight = {
-  hidden: { opacity: 0, x: 50 },
-  visible: { opacity: 1, x: 0, transition: { duration: 0.6 } },
-};
-
-const fadeUpStagger: Variants = {
-  hidden: { opacity: 0, y: 50 },
-  visible: () => ({
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.10},
-  }),
-};
+import { InViewAnimationWrapper } from "@/components/ui/InViewAnimationWrapper";
 
 export default function ServicesCardsSection() {
   return (
@@ -29,12 +12,11 @@ export default function ServicesCardsSection() {
           <div className="flex flex-col gap-10 self-start order-last table-lg:order-first">
             
             {/* Tarjeta pequeña */}
-            <motion.div
-              custom={0}
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.2 }}
-              variants={fadeUpStagger}
+            <InViewAnimationWrapper
+              direction="x"
+              offset={-20}
+              delay={0.2}
+              transition={{duration: 0.5}}
               className="
                 bg-colorSecundario4
                 ring-2 ring-white/20 hover:ring-colorSecundario2
@@ -69,15 +51,14 @@ export default function ServicesCardsSection() {
                   </div>
                 </div>
               </div>
-            </motion.div>
+            </InViewAnimationWrapper>
 
             {/* Tarjeta mediana */}
-            <motion.div
-              custom={1}
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.2 }}
-              variants={fadeUpStagger}
+            <InViewAnimationWrapper
+              direction="x"
+              offset={-20}
+              delay={0.2}
+              transition={{duration: 0.5}}
               className="
                 bg-colorSecundario4
                 ring-2 ring-white/20 hover:ring-colorSecundario2
@@ -114,15 +95,14 @@ export default function ServicesCardsSection() {
                   </div>
                 </div>
               </div>
-            </motion.div>
+            </InViewAnimationWrapper>
 
             {/* Tarjeta grande */}
-            <motion.div
-              custom={2}
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.2 }}
-              variants={fadeUpStagger}
+            <InViewAnimationWrapper
+              direction="x"
+              offset={-20}
+              delay={0.2}
+              transition={{duration: 0.5}}
               className="
                 bg-colorSecundario4
                 ring-2 ring-white/20 hover:ring-colorSecundario2
@@ -158,15 +138,15 @@ export default function ServicesCardsSection() {
                   </div>
                 </div>
               </div>
-            </motion.div>
+            </InViewAnimationWrapper>
           </div>
           
           {/* Lado derecho - Título y subtítulo */}
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            variants={fadeRight}
+          <InViewAnimationWrapper
+            direction="x"
+            offset={20}
+            delay={0.3}
+            transition={{duration: 0.5}}
             className="text-center table-lg:text-right order-first table-lg:order-last"
           >
             <h2 className="text-4xl md-tablet:text-7xl font-extrabold text-colorPrimario2 mb-8">
@@ -177,18 +157,16 @@ export default function ServicesCardsSection() {
               Desde la conceptualización (MVP y Startups) hasta el crecimiento (PyMEs) y la modernización de plataformas (Legacy), adaptamos nuestra experiencia para 
               <span className="text-colorSecundario4"> impulsar tu visión. </span>
             </p>
-          </motion.div>
+          </InViewAnimationWrapper>
         </div>
 
         {/* Call to action final */}
-        <motion.div
+        <InViewAnimationWrapper
           className="relative z-10 text-center mt-20"
-          initial={{ opacity: 0, y: 30 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          direction="y"
+          offset={20}
+          delay={0.3}
           transition={{ duration: 0.3,}}
-          viewport={{ once: true }}
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
         >
           <Link
             href="/contact"
@@ -200,22 +178,19 @@ export default function ServicesCardsSection() {
             </span>
             <Zap className="w-6 h-6 text-colorPrimario5 flex-shrink-0" />
           </Link>
-        </motion.div>
-
-
+        </InViewAnimationWrapper>
       </div>
 
-      <motion.div
-        whileHover={{ scale: 1.05, skewX: -5 }} // solo se aplica en desktop
-        transition={{ duration: 0.3 }}
+      {/* CTA para los servicios */}
+      <div
         className="
-          relative overflow-hidden
-          text-center py-12 px-8 my-20 shadow-2xl mx-auto select-none
+          relative overflow-hidden block
+          text-center py-12 px-8 my-20 shadow-2xl mx-auto select-none 
           bg-colorHover2
-          w-full cursor-pointer transform
-          md-tablet:hover:scale-105 md-tablet:hover:skew-x-[-5deg] // aplica solo en desktop
-          md-tablet:skew-x-0
-          
+          w-full cursor-pointer
+          transition-all duration-300 ease-in-out transform 
+          md-tablet:hover:scale-[1.05] 
+          md-tablet:hover:skew-x-[-5deg]
         "
       >
         {/* Glow animado */}
@@ -225,7 +200,7 @@ export default function ServicesCardsSection() {
           Tenemos una amplia <span className="text-colorDarkFondo2">gama de servicios. </span>  
           Escoge el ideal <span className="text-colorDarkFondo2">para ti</span>
         </h3>
-      </motion.div>
+      </div>
     </section>
   );
 }

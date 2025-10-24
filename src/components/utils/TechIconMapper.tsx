cb4ca8a5c5de1b957a2a8f5ae1618974ed0654bf
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-10-24 17:45:47 -0400

    mapeo de nuevos iconos

--- src/components/utils/TechIconMapper.tsx
+++ src/components/utils/TechIconMapper.tsx
@@ -2,15 +2,30 @@
 
 import React from 'react';
 import { IconType } from "react-icons";
+
+import { DiDjango } from "react-icons/di";
+import { PiFigmaLogoFill } from "react-icons/pi";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { TbBrandReactNative } from "react-icons/tb";
+
 import { 
   FaJsSquare, FaNode, FaReact, FaAngular, FaPython,
   FaSwift, FaJava, FaGitAlt, FaGithub, FaDocker, FaAws
 } from "react-icons/fa";
 
+
 import { 
   SiMongodb, SiNextdotjs, SiTailwindcss, SiTypescript, 
   SiVite, SiNestjs, SiMysql, SiPostgresql, SiKotlin, 
-  SiFlutter, SiVercel, SiKubernetes
+  SiFlutter, SiVercel, SiKubernetes, SiExpress,
+  SiDotnet,
+  SiCplusplus,
+  SiRust,
+  SiAdobexd,
+  SiTensorflow,
+  SiOpenai,
+  SiFastapi,
+  SiFlask,
 } from "react-icons/si";
 
 // 1. Mapeo de la clave de texto a la función del componente
@@ -19,7 +34,9 @@ const IconMap: { [key: string]: IconType } = {
   FaSwift, FaJava, FaGitAlt, FaGithub, FaDocker, FaAws,
   SiMongodb, SiNextdotjs, SiTailwindcss, SiTypescript, 
   SiVite, SiNestjs, SiMysql, SiPostgresql, SiKotlin, 
-  SiFlutter, SiVercel, SiKubernetes
+  SiFlutter, SiVercel, SiKubernetes, SiExpress, SiDotnet,
+  SiCplusplus, SiRust, SiAdobexd, SiTensorflow, SiOpenai, SiFastapi,
+  SiFlask, DiDjango, PiFigmaLogoFill, BiLogoPostgresql, TbBrandReactNative
 };
 
 interface TechIconProps {

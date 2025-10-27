89c3ba98ff2b1f1955420c21a80f6e59e19f8de1
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-10-27 09:04:32 -0400

    refactorizacion del componente heroServices slug, ya no se usa use client

--- src/sections/services/components/HeroSectionServices.tsx
+++ src/sections/services/components/HeroSectionServices.tsx
@@ -1,84 +1,89 @@
-"use client";
-
 import Link from "next/link";
 import { ServicesData } from "@/data/ServicesDetails";
-import { motion, Variants } from "framer-motion";
 import { Sparkles } from "lucide-react";
+import { CardInViewWrapper } from "@/components/ui/CardInViewWrapper";
+import { InViewAnimationWrapper } from "@/components/ui/InViewAnimationWrapper";
 
 interface HeroSectionProps {
   service: ServicesData;
 }
 
-const container = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.3, // retraso entre cada hijo
-    },
-  },
-};
-
-const fadeUp: Variants = {
-  hidden: { opacity: 0, y: 40 },
-  show: { opacity: 1, y: 0, transition: { duration: 0.3} },
-};
-
 export default function HeroSectionServices({ service }: HeroSectionProps) {
   return (
-    <motion.section
+    <section
       className="relative w-full pt-24 pb-16 bg-gradient-to-b from-colorPrimario5 to-white flex flex-col items-center text-center px-4 md-tablet:px-8 overflow-hidden"
-      variants={container}
-      initial="hidden"
-      animate="show"
     >
       {/* Sparkles decorativo */}
-      <motion.div
+      <CardInViewWrapper
+        direction="x"
+        offset={20}
+        transition={{duration: 0.2, delay: 0.3}}
         className="animate-pulse absolute top-8 right-8 text-colorFondo opacity-60"
-        initial={{ opacity: 0, scale: 0.7 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
       >
         <Sparkles className="w-10 h-10" />
-      </motion.div>
+      </CardInViewWrapper>
+
       {/* Título */}
-      <motion.h1
-        variants={fadeUp}
-        className="
-          text-4xl md-tablet:text-6xl table-lg:text-7xl font-extrabold mb-6 
-          text-white drop-shadow-lg"
+      <InViewAnimationWrapper
+        direction="y"
+        offset={20}
+        transition={{duration: 0.3, delay: 0.4}}
       >
-        {service.name}
-      </motion.h1>
+        <h1
+          className="
+            text-4xl md-tablet:text-6xl table-lg:text-7xl font-extrabold mb-6 
+            text-white drop-shadow-lg"
+        >
+          {service.name}
+        </h1>
+      </InViewAnimationWrapper>
       
       {/* Separador decorativo */}
-      <motion.div
-        variants={fadeUp}
-        className="mx-auto mb-8 w-24 h-2 rounded-full bg-gradient-to-r from-colorPrimario5 to-colorSecundario1 shadow-md"
-      />
+      <InViewAnimationWrapper
+        direction="y"
+        offset={20}
+        transition={{duration: 0.3, delay: 0.5}}
+      >
+        <div className="
+          mx-auto mb-8 w-24 h-2 rounded-full shadow-md
+          bg-gradient-to-r from-colorPrimario5 to-colorSecundario1"
+        />
+      </InViewAnimationWrapper>
 
       {/* Subtítulo */}
-      <motion.p
-        variants={fadeUp}
-        className="text-lg md-tablet:text-2xl table-lg:text-3xl text-colorPrimario1 mb-10 max-w-3xl mx-auto font-medium drop-shadow-sm"
+      <InViewAnimationWrapper
+        direction="y"
+        offset={20}
+        transition={{duration: 0.3, delay: 0.6}}
       >
-        {service.subtitle}
-      </motion.p>
+        <p className="
+          text-lg md-tablet:text-2xl table-lg:text-3xl text-colorPrimario1 
+          mb-10 max-w-3xl mx-auto font-medium drop-shadow-sm"
+        >
+          {service.subtitle}
+        </p>
+      </InViewAnimationWrapper>
       
       {/* Botón global */}
-      <motion.div variants={fadeUp} className="my-8 mx-10">
+      <InViewAnimationWrapper
+        direction="y"
+        offset={20}
+        transition={{duration: 0.3, delay: 0.1}}
+        className="my-8 mx-10"
+      >
         <Link href="/contact">
-          <motion.button
-            whileHover={{ scale: 1.07 }}
-            whileTap={{ scale: 0.97 }}
-            className="px-10 py-5 text-xl md:text-2xl font-bold bg-gradient-to-r from-colorPrimario5 to-colorSecundario1 text-white rounded-2xl shadow-xl hover:bg-colorSecundario4 transform transition-all duration-300 flex items-center gap-3 drop-shadow-lg"
+          <button
+            className="
+              px-10 py-5 text-xl md:text-2xl font-bold bg-gradient-to-r from-colorPrimario5 to-colorSecundario1 
+              text-white rounded-2xl shadow-xl hover:bg-colorSecundario4 transform transition-all duration-300 
+              flex items-center gap-3 drop-shadow-lg hover:scale-[1.05] active:scale-[0.98]"
           >
             <span className="inline-block w-3 h-3 rounded-full bg-colorPrimario5 animate-pulse"></span>
             Cotiza tu proyecto
             <span className="inline-block w-3 h-3 rounded-full bg-colorSecundario1 animate-pulse"></span>
-          </motion.button>
+          </button>
         </Link>
-      </motion.div>
-    </motion.section>
+      </InViewAnimationWrapper>
+    </section>
   );
 }

48d9f266b8f4de4998ead355e4e214958d421fcb
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-11-07 10:25:50 -0400

    Correccion de animaciones por nuevos componentes

--- src/sections/services/components/HeroSectionServices.tsx
+++ src/sections/services/components/HeroSectionServices.tsx
@@ -1,9 +1,9 @@
 import { ServicesData } from "@/data/ServicesDetails";
 import { Sparkles } from "lucide-react";
-import { CardInViewWrapper } from "@/components/ui/CardInViewWrapper";
-import { InViewAnimationWrapper } from "@/components/ui/InViewAnimationWrapper";
 import ButtonCta from "@/components/ui/ButtonCta";
 import { Rocket } from "lucide-react";
+import { SimpleInViewWrapper } from "@/components/ui/SimpleInViewWrapper";
+import TextRevealClient from "@/components/ui/TextRevealClient";
 
 interface HeroSectionProps {
   service: ServicesData;
@@ -15,32 +15,28 @@ export default function HeroSectionServices({ service }: HeroSectionProps) {
       className="relative w-full pt-24 pb-16 bg-gradient-to-b from-colorPrimario5 to-white flex flex-col items-center text-center px-4 md-tablet:px-8 overflow-hidden"
     >
       {/* Sparkles decorativo */}
-      <CardInViewWrapper
+      <SimpleInViewWrapper
         direction="x"
         offset={20}
-        transition={{duration: 0.2, delay: 0.3}}
+        transition={{duration: 0.4, delay: 0.3}}
         className="animate-pulse absolute top-8 right-8 text-colorFondo opacity-60"
       >
         <Sparkles className="w-10 h-10" />
-      </CardInViewWrapper>
+      </SimpleInViewWrapper>
 
       {/* Título */}
-      <InViewAnimationWrapper
+      <TextRevealClient
         direction="y"
         offset={20}
         transition={{duration: 0.3, delay: 0.4}}
       >
-        <h1
-          className="
-            text-5xl md-tablet:text-6xl table-lg:text-7xl font-extrabold mb-6 
-            text-white drop-shadow-lg"
-        >
+        <h1 className="text-5xl md-tablet:text-6xl table-lg:text-7xl font-extrabold mb-6 text-white drop-shadow-lg">
           {service.name}
         </h1>
-      </InViewAnimationWrapper>
+      </TextRevealClient>
       
       {/* Separador decorativo */}
-      <InViewAnimationWrapper
+      <SimpleInViewWrapper
         direction="y"
         offset={20}
         transition={{duration: 0.3, delay: 0.5}}
@@ -49,10 +45,10 @@ export default function HeroSectionServices({ service }: HeroSectionProps) {
           mx-auto mb-8 w-24 h-2 rounded-full shadow-md
           bg-gradient-to-r from-colorPrimario5 to-colorSecundario1"
         />
-      </InViewAnimationWrapper>
+      </SimpleInViewWrapper>
 
       {/* Subtítulo */}
-      <InViewAnimationWrapper
+      <TextRevealClient
         direction="y"
         offset={20}
         transition={{duration: 0.3, delay: 0.6}}
@@ -63,14 +59,14 @@ export default function HeroSectionServices({ service }: HeroSectionProps) {
         >
           {service.subtitle}
         </p>
-      </InViewAnimationWrapper>
+      </TextRevealClient>
       
       {/* Botón global */}
-      <InViewAnimationWrapper
+      <SimpleInViewWrapper
         direction="y"
         offset={20}
         transition={{duration: 0.3, delay: 0.2}}
-        className="my-8 mx-10"
+        className="my-8"
       >
         <ButtonCta
           href="/contact"
@@ -82,7 +78,7 @@ export default function HeroSectionServices({ service }: HeroSectionProps) {
         >
           Cotiza tu proyecto ahora
         </ButtonCta>
-      </InViewAnimationWrapper>
+      </SimpleInViewWrapper>
     </section>
   );
 }

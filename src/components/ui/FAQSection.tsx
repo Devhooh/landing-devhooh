5f6b97cfee9891b12ec638493c9a77c9f373cdb4
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-11-07 09:52:24 -0400

    Uso del nuevo componente y ligeros mejoras del componente FAQSection

--- src/components/ui/FAQSection.tsx
+++ src/components/ui/FAQSection.tsx
@@ -1,9 +1,11 @@
-"use client";
+
+"use client"
 
 import React, { useState } from 'react';
-import { ChevronDown, ChevronUp } from 'lucide-react';
-import { motion, Variants } from 'framer-motion';
 import TextRevealClient from './TextRevealClient';
+import { FAQItemSection } from '../layout/FAQItemSection';
+import { SimpleInViewWrapper } from './SimpleInViewWrapper';
+import ButtonCta from './ButtonCta';
 
 type FAQItemProps = {
   id: number;
@@ -13,64 +15,12 @@ type FAQItemProps = {
 
 interface listFAQ {
   itemsDate: FAQItemProps[]
+  title?: string;
+  description?: string;
 }
 
-// Animación para cards
-const faqItemVariants: Variants = {
-  hidden: { opacity: 0, y: 30 },
-  visible: (i: number) => ({
-    opacity: 1,
-    y: 0,
-    transition: { delay: i * 0.1, duration: 0.5, ease: "easeOut" },
-  })
-};
-
-// Componente FAQItem con motion
-const FAQItem: React.FC<{
-  question: string;
-  answer: string;
-  isOpen: boolean;
-  onClick: () => void;
-  index: number;
-}> = ({ question, answer, isOpen, onClick, index }) => {
-  return (
-    <motion.div
-      custom={index}
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      variants={faqItemVariants}
-      className="bg-white border border-colorPrimario3 rounded-xl shadow-sm hover:shadow-lg transition-shadow duration-300"
-    >
-      <button
-        onClick={onClick}
-        className="flex justify-between items-center w-full p-4 table-lg:p-6 text-left"
-      >
-        <span className="text-lg sm:text-xl font-semibold text-colorDarkFondo1 m-1">
-          {question}
-        </span>
-        {isOpen ? 
-          <ChevronUp className="h-7 w-7 md-tablet:h-8 md-tablet:w-8 text-colorPrimarioLogo1 shrink-0" /> 
-          : 
-          <ChevronDown className="h-7 w-7 md-tablet:h-8 md-tablet:w-8 text-colorPrimarioLogo1 shrink-0" />
-        }
-      </button>
-
-      {/* Animación de apertura/cierre */}
-      <motion.div
-        initial={{ height: 0, opacity: 0 }}
-        animate={isOpen ? { height: "auto", opacity: 1 } : { height: 0, opacity: 0 }}
-        transition={{ duration: 0.4, ease: "easeOut" }}
-        className="px-4 sm:px-6 overflow-hidden"
-      >
-        <p className="text-sm sm:text-base text-gray-600 py-2 pb-5">{answer}</p>
-      </motion.div>
-    </motion.div>
-  );
-};
-
 // Componente principal
-export default function FAQSection({itemsDate}: listFAQ ) {
+export default function FAQSection({title, description, itemsDate}: listFAQ ) {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
 
   const handleToggle = (index: number) => {
@@ -79,7 +29,7 @@ export default function FAQSection({itemsDate}: listFAQ ) {
 
   return (
     <section className="bg-colorHover5 pb-24 pt-8 sm:px-6 table-lg:px-8 flex items-center justify-center">
-      <div className="w-full max-w-4xl mx-auto px-10">
+      <div className="w-full max-w-4xl mx-auto px-5 md-tablet:px-10">
         <div className="text-center mb-12">
 
           <TextRevealClient
@@ -87,8 +37,8 @@ export default function FAQSection({itemsDate}: listFAQ ) {
             offset={30}
             transition={{duration: 0.3, delay: 0.3, ease: "easeInOut"}}
           >
-            <h1 className="text-xl md-tablet:text-4xl table-lg:text-5xl font-extrabold text-black mb-4">
-              Preguntas frecuentes
+            <h1 className="text-2xl md-tablet:text-4xl table-lg:text-5xl font-extrabold text-black mb-4">
+              {title ? title : "Preguntas frecuentes"}
             </h1>
           </TextRevealClient>
 
@@ -97,14 +47,17 @@ export default function FAQSection({itemsDate}: listFAQ ) {
             offset={30}
             transition={{duration: 0.3, delay: 0.4}}
           >
-            <p className="text-base md-tablet:text-lg text-gray-600 max-w-2xl mx-auto">
-              ¿Tiene preguntas sobre nuestros servicios digitales? Explore nuestra sección de preguntas frecuentes o póngase en contacto con nosotros directamente para obtener asistencia e información personalizadas.
+            <p className="text-lg md-tablet:text-xl text-gray-600 max-w-2xl mx-auto">
+              {description 
+                ? description 
+                : "¿Tiene preguntas sobre nuestros servicios digitales? Explore nuestra sección de preguntas frecuentes o póngase en contacto con nosotros directamente para obtener asistencia e información personalizadas."
+                }
             </p>
           </TextRevealClient>
         </div>
         <div className="space-y-4">
           {itemsDate.map((item, index) => (
-            <FAQItem
+            <FAQItemSection
               key={item.id}
               question={item.question}
               answer={item.answer}
@@ -113,6 +66,30 @@ export default function FAQSection({itemsDate}: listFAQ ) {
               index={index} // para el delay de aparición
             />
           ))}
+        </div>
+
+        <div
+          className="mt-16 text-center"
+        >
+          <TextRevealClient
+            direction="y"
+            offset={30}
+            transition={{duration: 0.3, delay: 0.3, ease: "easeInOut"}}
+          >
+            <p className="text-colorPrimario1/70 mb-4 font-medium text-2xl">
+              ¿Ya tienes una idea en mente?
+            </p>
+          </TextRevealClient>
+
+          <SimpleInViewWrapper>
+            <ButtonCta
+              href="/contact"
+              className="text-colorSecundario1 hover:text-colorSecundario3 text-xl
+                font-semibold underline underline-offset-4 hover:underline-offset-8 transition-all duration-300
+                hover:scale-[1.05] active:scale-[0.98] transform"
+            >
+              Empezemos con tu proyecto            </ButtonCta>
+          </SimpleInViewWrapper>
         </div>
       </div>
     </section>

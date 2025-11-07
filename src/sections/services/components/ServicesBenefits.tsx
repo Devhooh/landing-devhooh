122205202299a37f363bb634375f25d051e5f935
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-11-07 15:53:45 -0400

    se agrego nuevos componentes de animacion

--- src/sections/services/components/ServicesBenefits.tsx
+++ src/sections/services/components/ServicesBenefits.tsx
@@ -1,9 +1,8 @@
 import * as Icons from "lucide-react";
 import { LucideProps } from "lucide-react";
 import { ServicesData } from "@/data/ServicesDetails";
-import { InViewAnimationWrapper } from "@/components/ui/InViewAnimationWrapper";
-import StaggerListContainer from "@/components/ui/StaggerListContainer";
-import { StaggerListItemClient } from "@/components/ui/StaggerListItemClient";
+import TextRevealClient from "@/components/ui/TextRevealClient";
+import CardInViewStagger from "@/components/ui/CardInViewStagger";
 
 interface BenefitsSectionProps {
   service: ServicesData;
@@ -13,23 +12,23 @@ export default function ServicesBenefits({ service }: BenefitsSectionProps) {
   return (
     <section className="p-6 md-tablet:px-8 table-lg:px-12 bg-blue-50">
       <div className="w-auto px-auto">
-
-        <InViewAnimationWrapper
+        <TextRevealClient
           direction="x"
           offset={-20}
           transition={{duration: 0.3, delay: 0.4}}
         >
           <h2 className="text-3xl font-extrabold mb-5 text-center md-tablet:text-left text-colorPrimario2">
-            Beneficios de nuestro servicio de
-            <br/>
+            Beneficios del{" "}
             <strong className="text-colorPrimario5">
             {service.name}
             </strong>
           </h2>
-        </InViewAnimationWrapper>
+        </TextRevealClient>
       </div>
 
-      <StaggerListContainer className="grid grid-cols-1 md-tablet:grid-cols-2 gap-6 table-lg:gap-8">
+      <div 
+        className="grid grid-cols-1 md-tablet:grid-cols-2 gap-6 table-lg:gap-8"
+      >
         {service.benefits.map((benefit, index) => {
           const Icon =
             (Icons[benefit.icon as keyof typeof Icons] as React.ComponentType<
@@ -37,8 +36,9 @@ export default function ServicesBenefits({ service }: BenefitsSectionProps) {
             >) || Icons.HelpCircle;
 
           return (
-            <StaggerListItemClient
+            <CardInViewStagger
               key={index}
+              index={index + 1}
               direction="y"
               offset={60}
               className="flex flex-col md-tablet:flex-row items-start md-tablet:items-center bg-white p-6 rounded-2xl shadow-md border border-fuchsia-300 hover:shadow-lg transition"
@@ -53,10 +53,10 @@ export default function ServicesBenefits({ service }: BenefitsSectionProps) {
                 <h3 className="text-lg font-semibold mb-1">{benefit.title}</h3>
                 <p className="text-gray-600 text-sm">{benefit.description}</p>
               </div>
-            </StaggerListItemClient>
+            </CardInViewStagger>
           );
         })}
-      </StaggerListContainer>
+      </div>
     </section>
   );
 }

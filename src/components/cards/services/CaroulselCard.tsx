56e8ea3291a42fe07995ad75e158ee186bba43a3
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-11-08 11:04:49 -0400

    Uso de los nuevos botones loader en el CaroulselCard

--- src/components/cards/services/CaroulselCard.tsx
+++ src/components/cards/services/CaroulselCard.tsx
@@ -1,10 +1,5 @@
-"use client"
-
 import Image from "next/image";
-import Link from "next/link";
-import React, { useState } from 'react';
-import { Loader2 } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import ServiceCardButtons from "@/components/layout/ServiceCardButtons";
 
 interface CardData {
   slug: string;
@@ -20,16 +15,6 @@ export default function CaroulselCard({
   index?: number;
 }) {
 
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleNavigation = (path: string) => {
-      setIsLoading(true);
-      setTimeout(() => {
-          router.push(path);
-      }, 50);
-  };
-
   return (
     <div
       className="
@@ -37,12 +22,6 @@ export default function CaroulselCard({
         rounded-2xl shadow-xl overflow-hidden h-[490px] flex flex-col 
         hover:scale-[1.05] active:scale-[0.98] transform transition-all duration-300"
     >
-      {isLoading && (
-        <div className="absolute inset-0 bg-white/70 backdrop-blur-sm z-20 flex items-center justify-center rounded-2xl">
-          <Loader2 className="w-10 h-10 text-colorPrimario5 animate-spin" />
-        </div>
-      )}
-
       {/* Imagen */}
       <div className="flex items-center justify-center p-6 h-40 md-tablet:h-48">
         <Image
@@ -67,30 +46,7 @@ export default function CaroulselCard({
       </div>
 
       {/* Botones */}
-      <div className="mb-12 px-5 flex flex-col gap-4">
-        <Link href="/contact" passHref legacyBehavior>
-          <button 
-            onClick={() => handleNavigation("/contact")}
-            className="
-              relative w-full py-2 rounded-lg bg-colorPrimario5 text-white border border-colorSecundario2 transform delay-100
-              font-semibold overflow-hidden hover:bg-colorPrimario5/70 transition-colors"
-          >
-            <span className="relative z-10">Cotiza tu proyecto</span>
-            <span className="absolute inset-0 bg-white/30 -translate-x-full rotate-45 animate-shine" />
-          </button>
-        </Link>
-
-        <Link href={`/services/${card.slug}`} passHref legacyBehavior>
-          <button
-            onClick={() => handleNavigation(`/services/${card.slug}`)}
-            className="
-              w-full py-2 rounded-lg border border-colorSecundario2 text-colorPrimario4 font-medium bg-white 
-              hover:bg-colorPrimario5/30 transition-colors transform delay-100"
-          >
-            Ver más detalles
-          </button>
-        </Link>
-      </div>
+      <ServiceCardButtons slug={card.slug}/>
     </div>
   );
 }
